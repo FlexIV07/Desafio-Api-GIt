d479d08f24b14fcb971cb55c5e08bb7e4270b973
FlexIV07 <caiofelix2002@hotmail.com>
2023-10-03 19:39:49 -0300

    fix: perfil usuario

--- report_builder.py
+++ report_builder.py
@@ -8,4 +8,4 @@ class Report:
         user_data = self.__user_info[0]
         repo_data = self.__user_info[1]
         with open(f"{user_data.name}.txt", 'w') as report:
-            report.write(f'Nome: {user_data.name}\nNúmero de repositórios publicos: {user_data.public_repos}\nNúmero de seguidores: {user_data.followers}\nNúmero de usuários seguidos: {user_data.following}\nLista de Repositórios: {repo_data}')
+            report.write(f'Nome: {user_data.name}\nPerfil: {user_data.html_url}\nNúmero de repositórios publicos: {user_data.public_repos}\nNúmero de seguidores: {user_data.followers}\nNúmero de usuários seguidos: {user_data.following}\nLista de Repositórios: {repo_data}')
